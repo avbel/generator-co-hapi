2236656329a6279a2bce662961b548654ce8aed5
Andrey Belchikov <belchikovs@gmail.com>
2014-09-04 00:30:37 -0700

    Repaired existing tests

--- test/app.js
+++ test/app.js
@@ -58,23 +58,14 @@ describe('co-hapi generator', function () {
       cacheEngineOptions: "{\"test1\": true}",
       host: "my-host",
       port: "8080",
-      serverOptions: "{\"serverOptions\": \"test\"}",
-      plugins: "foo, bar"
+      serverOptions: "{\"serverOptions\": \"test\"}"
     });
     this.app.run({}, function () {
-      var pkg = JSON.parse(fs.readFileSync(path.join(__dirname, 'temp', 'package.json'), 'utf8'));
-      assert(pkg.dependencies['foo']);
-      assert(pkg.dependencies['bar']);
-      assert(pkg.dependencies['catbox-memory']);
       var config = JSON.parse(fs.readFileSync(path.join(__dirname, 'temp', 'composer.json'), 'utf8'));
       assert(config.pack.cache.engine === 'catbox-memory');
       assert(config.pack.cache.test1 === true);
       assert(config.servers[0].host === 'my-host');
       assert(config.servers[0].port === 8080);
-      assert(config.servers[0].options);
-      assert(config.servers[0].options.serverOptions === 'test');
-      assert(config.plugins['foo']);
-      assert(config.plugins['bar']);
       done();
     });
   });
